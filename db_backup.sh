6f34a80aea84dc36ea3515849b8a7f5f19da2f09
Mounir <messelmeni.mounir@gmail.com>
2013-06-24 14:33:21 +0200

    Update db_backup.sh

--- db_backup.sh
+++ db_backup.sh
@@ -2,6 +2,6 @@
 # replace the postgres with Database username and the dbname with the Database name
 # save it to a tar file
 cd /var/Dropbox_db_backup/
-pg_dump -U postgres -Fc tvams > $(date "+%Y%m%d.tar.gz")
+pg_dump -U postgres -Fc dbname > $(date "+%Y%m%d.tar.gz")
 # upload the DB backup to Dropbox
 python uploader.py
